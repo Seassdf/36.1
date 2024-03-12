2bde0069d9f447c890d989dc850119fcea0a8083
tyhzal09@gmail.com <tyhzal09@gmail.com>
2024-03-12 18:47:18 +0300

    qwe

---
+++ 36.1/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+void testingTask01();
+void testingTask02();
+
+
+int main() {
+	testingTask01();
+	testingTask02();
+	/*testingTask03();
+	testingTaskX();
+	return 0;*/
+}
